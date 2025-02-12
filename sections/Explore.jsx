3f18618ab388d0ad5864ec935740fd0dd884b396
Jahez Abraham Johny <jaj@sureflow.com>
2025-02-12 12:38:16 +0400

    Hero + Explore section update

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,9 +1,36 @@
-'use client';
+"use client";
 
-const Explore = () => (
-  <section>
-    Explore section
-  </section>
-);
+import { useState } from "react";
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { staggerContainer } from "../utils/motion";
+import { TypingText, ExploreCard, TitleText } from "../components";
+import { exploreWorlds } from "../constants/index";
+
+const Explore = () => {
+  return (
+    <section className={`${styles.paddings}`} id="explore">
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title="| The World" textStyles="text-center" />
+        <TitleText
+          title={
+            <>
+              Choose the world you want <br className="md:block hidden" /> to
+              explore
+            </>
+          }
+          textStyles="text-center"
+        />
+      </motion.div>
+      Explore section
+    </section>
+  );
+};
 
 export default Explore;
